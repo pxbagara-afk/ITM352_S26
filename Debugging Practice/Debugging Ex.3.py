eebbf63ecbc675ab01c122788e2b3f683a437c8e
pxbagara-afk <pxbagara@hawaii.edu>
2026-03-11 21:37:12 -1000

    Debugging assignment

---
+++ Debugging Practice/Debugging Ex.3.py
@@ -0,0 +1,8 @@
+# Function for multiplying numbers
+def multiply(x, y):
+    return x * y
+    
+x = 5
+y = 6
+result = multiply(x,y)
+print(result)
---
+++ Debugging Practice/Debugging Ex4.py
@@ -0,0 +1,9 @@
+# Retrieving elements from a list
+def get_element(list, index):
+    return list[index]
+
+
+my_list = [1, 2, 3, 4, 5]
+print(get_element(my_list, 2))  
+print(get_element(my_list, 4)) 
+
